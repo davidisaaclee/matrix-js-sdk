5bb526693a480493ad96655057c8e10344206400
David Lee <david.isaac.lee@gmail.com>
2022-12-30 21:12:49 -0800

    [wip] try pushing built package

---
+++ lib/@types/read_receipts.d.ts
@@ -0,0 +1,10 @@
+export declare enum ReceiptType {
+    Read = "m.read",
+    FullyRead = "m.fully_read",
+    ReadPrivate = "m.read.private",
+    /**
+     * @deprecated Please use the ReadPrivate type when possible. This value may be removed at any time without notice.
+     */
+    UnstableReadPrivate = "org.matrix.msc2285.read.private"
+}
+//# sourceMappingURL=read_receipts.d.ts.map
